7643011115925ad98edb1f3119aa0bc6ff063787
Roger Woo <rogerywoo@gmail.com>
2022-05-14 01:54:41 -0400

    Next Project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,7 @@
-import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-import Welcome from "./page/Welcome";
-import Products from "./page/Products";
-import MainHeader from "./component/MainHeader";
-import ProductDetail from "./page/ProductDetail";
-import { Redirect } from "react-router-dom";
-
-
 function App() {
   return (
     <div>
-      <MainHeader>
 
-      </MainHeader>
-      <main>
-        <Switch>
-          <Route path="/" exact >
-            <Redirect to="/welcome" />
-          </Route>
-
-          {/* All routes will checked.  Not just one.  Switch component will act as a switch */}
-          <Route path="/welcome">
-            <Welcome></Welcome>
-          </Route>
-          {/* The product detail needs to be before the products page or use exact */}
-          <Route path="/products/:productId">
-            <ProductDetail />
-          </Route>
-          {/* You can use <Route path="/producst" exact> in order to force exact match  */}
-          <Route path="/products">
-            <Products></Products>
-          </Route>
-        </Switch>
-      </main>
     </div>
   );
 }
